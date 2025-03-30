51ec33232c183b9c38eb562d0e58d8c34d2229f0
alshakibeelahi <alshakibeelahi@gmail.com>
2025-03-30 14:00:46 +0600

    routing init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { StudentComponent } from './components/student/student.component';
+import { UniversityComponent } from './components/university/university.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "university",
+  },
+  {
+    path: "student",
+    component: StudentComponent,
+  },
+  {
+    path: "university",
+    component: UniversityComponent,
+  },
+  {
+    path:"**", // Wildcard route for a 404 page
+    redirectTo: "/",
+  }
+];
